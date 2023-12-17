34e27737afec79f84f1b4caf1d3a29ff5841744c
roslynpius <roslyn.pius@gmail.com>
2023-12-17 15:40:27 +0530

    Initial Commit

---
+++ src/com/bridgelabz/cabinvoicegenerator/Main.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class Main{
+    public static void main(String[] args)
+    {
+        System.out.println("Cab Invoice Generator Implementation");
+    }
+}
